a7544608af6d9de1ecd9ef8616d5f703b15e140c
akhilbharambe <akhilbharambe444@gmail.com>
2023-12-18 16:05:38 +0530

    quizz app

--- quizz_app/src/loading.js
+++ quizz_app/src/loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
   return (
     <main>
-      <div className='loading'>Loading....</div>
+      <div className='loading'></div>
     </main>
   )
 }
